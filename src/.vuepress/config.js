d2854f689929670567f03c73bde8ba6efd0f158b
YuyaMiyaji <yuya.382@mail.benesse.co.jp>
2022-09-28 23:39:43 +0900

    commit

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -21,7 +21,8 @@ module.exports = {
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
   ],
-  dest: 'dist',
+  dest: 'docs/',
+  base: '/guide/',
   /**
    * Theme configuration, here is the default theme configuration for VuePress.
    *
